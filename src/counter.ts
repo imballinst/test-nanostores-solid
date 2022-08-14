408945a55ca3a7c943b003fedc944059c8480187
Try Ajitiono <try.ajitiono@accelbyte.net>
2022-08-14 16:40:13 +0700

    initial commit

---
+++ src/counter.ts
@@ -0,0 +1,15 @@
+import { useStore } from "@nanostores/solid";
+import { atom } from "nanostores";
+
+const store = atom(1);
+useStore(store);
+
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0;
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(++counter));
+  setCounter(0);
+}
